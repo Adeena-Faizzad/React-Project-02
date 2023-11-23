6a82cd457dad9d689d8c336260f1d4d514f514a6
“Adeena-Faizzad” <130522128+Adeena-Faizzad@users.noreply.github.com>
2023-11-24 00:13:28 +0300

    Derived state

---
+++ src/components/Log.jsx
@@ -0,0 +1,12 @@
+import { Linter } from 'eslint'
+import React from 'react'
+
+function Log({turns}) {
+  return (
+    <ol id='log'>
+        { turns.map(turn => <li key={`${turn.square.row} ${turn.square.row} `}>{turn.player} selected {turn.square.row} , {turn.square.col} </li>) }
+    </ol>
+  )
+}
+
+export default Log
